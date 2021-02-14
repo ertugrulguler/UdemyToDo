d99b3c6721d7fd52a4ce23a8f97d91922133eabb
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2021-02-14 23:00:07 +0300

    rapor tablosu mapping

---
+++ YSKProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Mapping/RaporMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.Entities.Concrete;
+
+namespace YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Mapping
+{
+    public class RaporMap : IEntityTypeConfiguration<Rapor>
+    {
+        public void Configure(EntityTypeBuilder<Rapor> builder)
+        {
+            builder.HasKey(i => i.Id);
+            builder.Property(i => i.Id).UseIdentityColumn();
+            builder.Property(i => i.Tanim).HasMaxLength(100).IsRequired();
+            builder.Property(i => i.Detay).HasColumnType("ntext");
+
+            builder.HasOne(i => i.Gorev).WithMany(i => i.Raporlar).HasForeignKey(i => i.GorevId);
+        }
+    }
+}
