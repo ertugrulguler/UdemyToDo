42c719a21ce5fbd5a9e3bb1e4688d52c047c4ffb
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2021-02-14 23:00:30 +0300

    Context tanımlaması

--- YSKProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Contexts/TodoContext.cs
+++ YSKProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Contexts/TodoContext.cs
@@ -8,7 +8,7 @@ using YSKProje.ToDo.Entities.Concrete;
 
 namespace YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts
 {
-    public class TodoContext : IdentityDbContext<AppUser,AppRole,int>
+    public class TodoContext : IdentityDbContext<AppUser, AppRole, int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -19,14 +19,18 @@ namespace YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Contexts
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           
+
             modelBuilder.ApplyConfiguration(new GorevMap());
+            modelBuilder.ApplyConfiguration(new AciliyetMap());
+            modelBuilder.ApplyConfiguration(new RaporMap());
+            modelBuilder.ApplyConfiguration(new AppUserMap());
 
             base.OnModelCreating(modelBuilder);
         }
 
-      
+
         public DbSet<Gorev> Gorevler { get; set; }
         public DbSet<Aciliyet> Aciliyetler { get; set; }
+        public DbSet<Rapor> Raporlar { get; set; }
     }
 }
