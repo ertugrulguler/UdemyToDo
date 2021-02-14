31cb0a9920f4f0c7b4836415b3c5858ea42291ca
Ertuğrul Güler <ertugrulguler@outlook.com.tr>
2021-02-14 22:30:00 +0300

    Add project files.

---
+++ YSKProje.ToDo.DataAccess/Concrete/EntityFrameworkCore/Mapping/AppUserMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YSKProje.ToDo.Entities.Concrete;
+
+namespace YSKProje.ToDo.DataAccess.Concrete.EntityFrameworkCore.Mapping
+{
+    public class AppUserMap : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(i => i.Name).HasMaxLength(100);
+            builder.Property(i => i.Surname).HasMaxLength(100);
+
+            builder.HasMany(i => i.Gorevler).WithOne(i => i.AppUser).HasForeignKey(i => i.AppUserId).OnDelete(DeleteBehavior.SetNull);
+            //çoka tek ilişkide çok olan kısımdan hasmany ile başlanır. OnDelete metodunda, eğer bir kullanıcı silinirse onun görevleri silinmesin
+            //kullanıcı id kısmı null olarak atansın olarak düzenlenmiştir.
+        }
+    }
+}
---
+++ YSKProje.ToDo.Entities/Concrete/Gorev.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using YSKProje.ToDo.Entities.Interfaces;
+
+namespace YSKProje.ToDo.Entities.Concrete
+{
+    public class Gorev : ITablo
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Aciklama { get; set; }
+        public bool Durum { get; set; }
+        public DateTime OlusturulmaTarih { get; set; }
+
+        public int? AppUserId { get; set; } //görec önce oluşturulup sonra birine atama yapılabilir
+        public AppUser AppUser { get; set; }
+    }
+}
