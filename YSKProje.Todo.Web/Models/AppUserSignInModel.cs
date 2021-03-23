0dfd1e3684e929ec824a30a6984ea19d7342e971
Ertuğrul Güler <ertglr.61@gmail.com>
2021-03-23 23:00:55 +0300

    Giriş yap sayfasının oluşturulması

---
+++ YSKProje.Todo.Web/Models/AppUserSignInModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YSKProje.Todo.Web.Models
+{
+    public class AppUserSignInModel
+    {
+        [Required(ErrorMessage = "Kullanıcı adı boş geçilemez.")]
+        [Display(Name = "Kullanıcı adı:")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Şifre boş geçilemez.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Şifre:")]
+        public string Password { get; set; }
+    }
+}
