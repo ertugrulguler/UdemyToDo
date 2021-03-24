be147061ca46d30ddb0175cbb3280d456ceb105b
Ertuğrul Güler <ertglr.61@gmail.com>
2021-03-24 22:13:59 +0300

    authorize

--- YSKProje.Todo.Web/Areas/Admin/Controllers/AciliyetController.cs
+++ YSKProje.Todo.Web/Areas/Admin/Controllers/AciliyetController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using YSKProje.Todo.Web.Areas.Admin.Models;
 using YSKProje.ToDo.Business.Interfaces;
@@ -6,6 +7,7 @@ using YSKProje.ToDo.Entities.Concrete;
 
 namespace YSKProje.Todo.Web.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "Admin")]
     [Area("Admin")]
     public class AciliyetController : Controller
     {
