3898d73bf53f824483deb658e200618b6bc3a18e
Ertuğrul Güler <ertglr.61@gmail.com>
2021-05-08 02:04:30 +0300

    iş emirleri sayfası tasarım güncellemeleri ve default resim basılması

--- YSKProje.ToDo.Entities/Concrete/AppUser.cs
+++ YSKProje.ToDo.Entities/Concrete/AppUser.cs
@@ -6,11 +6,11 @@ using YSKProje.ToDo.Entities.Interfaces;
 
 namespace YSKProje.ToDo.Entities.Concrete
 {
-    public class AppUser : IdentityUser<int>,ITablo
+    public class AppUser : IdentityUser<int>, ITablo
     {
         public string Name { get; set; }
         public string Surname { get; set; }
-        public string Picture { get; set; }
+        public string Picture { get; set; } = "default.png";
 
         public List<Gorev> Gorevler { get; set; }
     }
